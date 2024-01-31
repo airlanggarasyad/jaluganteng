fc024da9fdf9fb10cea69b4eae1bf83f0c422a7a
airlangga fidiyanto <44868652+airlanggarasyad@users.noreply.github.com>
2024-01-31 09:20:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: "Hello BLFP 9" })
 })
 
+app.get('/ganteng', (req, res) => {
+  res.status(200).json({ message: "Hello Jalu Ganteng Ganteng Sekali" })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port} ${process.env.NODE_ENV} ${process.env.PORT}`)
 })
